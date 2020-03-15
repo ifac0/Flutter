fcdf6b609be0bc1125efd0699feb4bea92351a9e
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-15 13:06:47 -0300

    feat: :hammer: Troca nome da pasta;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- flutter_fundamentos/ios/Flutter/flutter_export_environment.sh
+++ flutter_fundamentos/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/ifac0/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ifac0/stdy/Flutter/flutter-fundamentos"
+export "FLUTTER_APPLICATION_PATH=/home/ifac0/stdy/Flutter/flutter_fundamentos"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
