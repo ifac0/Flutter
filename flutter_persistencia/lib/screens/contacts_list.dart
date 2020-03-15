00f7c3837aa380ea4ae8ae9a8530e3f3830e3123
Ivan Costa <ivan.costa@justa.com.vc>
2020-03-15 11:58:32 -0300

    feat: :art: Melhora o loading observando o ciclo de vida do FutureBuilder;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

--- flutter_persistencia/lib/screens/contacts_list.dart
+++ flutter_persistencia/lib/screens/contacts_list.dart
@@ -10,28 +10,37 @@ class ContactsList extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Contatos'),),
       body: FutureBuilder(
-        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        future: findAll(),
         builder: (context, snapshot) {
-          if( snapshot.data == null){
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  CircularProgressIndicator(),
-                  Text('Carregando'),
-                ],
-              ),
-            );
+          switch(snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+              break;
           }
-          final List<Contact> contacts = snapshot.data;
-          return ListView.builder(
-            itemBuilder: (context, index) {
-              final Contact contact = contacts[index];
-              return _ContactItem(contact);
-            },
-            itemCount: contacts.length,
-          );
+          return Text('Unkown error');;
         },),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
